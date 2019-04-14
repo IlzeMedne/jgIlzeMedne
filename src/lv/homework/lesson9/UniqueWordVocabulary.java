b5bb09a60ed8f7c414bbfcc9ba9a5b7055f58bb5
Ilze <ilzemedne.lv@gmail.com>
2019-04-14 22:13:57 +0300

    Lesson9

---
+++ src/lv/homework/lesson9/UniqueWordVocabulary.java
@@ -0,0 +1,30 @@
+package lv.homework.lesson9;
+
+import java.util.*;
+
+public class UniqueWordVocabulary {
+    public static void main(String[] args) {
+
+        Set<String> words = new HashSet<>();
+
+        words.add("dog");
+        words.add("elephant");
+        words.add("monkey");
+        words.add("rat");
+        words.add("lion");
+        words.add("cat");
+        words.add("dog");
+        words.add("mouse");
+        words.add("lion");
+        words.add("fox");
+        words.add("wolf");
+
+        System.out.println("Count of unique words: " + words.size());
+        System.out.println("Set: " + words);
+
+        // īsti nesaprotu, kas uzdevumā ir prasīts.
+        // Set parāda un skaita tikai unikālās vērtības.
+        // vai tiešām tas bija viss, kas te jādara un jāsaprot?
+        // vai to printēšanu un skaitīšanu pārtaisīt kā metodes?
+    }
+}
