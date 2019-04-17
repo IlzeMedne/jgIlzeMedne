82532ef1bc87c08aa2abc3bf3a6afcd82fb528a3
Ilze <ilzemedne.lv@gmail.com>
2019-04-17 20:59:42 +0300

    Lesson10

---
+++ src/lv/homework/lesson10/UserValidationService.java
@@ -0,0 +1,31 @@
+package lv.homework.lesson10;
+
+public class UserValidationService {
+
+    private String name;
+    private String lastName;
+    private Integer age;
+
+    public UserValidationService(String name, String lastName, Integer age) throws ValidationException {
+        if (name.length() < 3 || name.length() > 15) {
+            throw new ValidationException("Name length should be 3 - 15 characters");
+        }
+        this.name = name;
+        if (lastName.length() < 3 || lastName.length() > 15) {
+            throw new ValidationException("Last Name length should be 3 - 15 characters");
+        }
+        this.lastName = lastName;
+        if (age < 0 || age > 120) {
+            throw new ValidationException("Age can not be less than 0 and more than 120");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " +
+                "\nName:" + name +
+                "\nLast Name: " + lastName +
+                "\nage: " + age;
+    }
+}
---
+++ src/lv/homework/lesson10/UserValidationServiceTest.java
@@ -0,0 +1,11 @@
+package lv.homework.lesson10;
+
+public class UserValidationServiceTest {
+
+    public static void main(String[] args) {
+
+        UserValidationService user1 = new UserValidationService("Ilze", "M", 120);
+
+        System.out.println(user1);
+    }
+}
